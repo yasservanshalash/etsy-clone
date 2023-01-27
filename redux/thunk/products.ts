56360670de1bc9c6aea5c800dde6493997d15586
Yasser Shalash <yasservanshalash@gmail.com>
2023-01-27 19:01:18 +0100

    testing local api

--- redux/thunk/products.ts
+++ redux/thunk/products.ts
@@ -2,9 +2,9 @@ import { Product } from "../../types/types";
 import { productActions } from "../slices/productsSlice";
 import { AppDispatch } from "../store";
 
-// const url = "http://localhost:3000/api/products";
-const url = "https://fakestoreapi.com/products";
-
+const url = "/api/products";
+// const url = "https://fakestoreapi.com/products";
+// const url = "https://etsy-clone-nine.vercel.app/api/products"
 export function fetchProductData() {
     return async (dispatch: AppDispatch) => {
         const response = await fetch(url);
